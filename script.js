23c3ea40996352157217d51e89571f143a9f031c
Sewes le Roux <31251956+sleroux0217@users.noreply.github.com>
2024-01-31 15:49:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const userInput = document.getElementById('text-input');
+const validateButton = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+const checkForPalindrome = input => {
+  const originalInput = input; // Store for later output
+
+  if (input === '') {
+    alert('Please input a value');
+    return;
+  }
+
+  // Remove the previous result
+  result.replaceChildren();
+
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+  let resultMsg = `<strong>${originalInput}</strong> ${
+    lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+  } a palindrome.`;
+
+  const pTag = document.createElement('p');
+  pTag.className = 'user-input';
+  pTag.innerHTML = resultMsg;
+  result.appendChild(pTag);
+
+  // Show the result.
+  result.classList.remove('hidden');
+};
+
+validateButton.addEventListener('click', () => {
+  checkForPalindrome(userInput.value);
+  userInput.value = '';
+});
+
+userInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') { // Change 'enter' to 'Enter'
+    checkForPalindrome(userInput.value); // Change to match function name
+    userInput.value = '';
+  }
+});
 
